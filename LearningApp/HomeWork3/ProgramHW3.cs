ab4f3d0129fabda6d998780d8ed9cda75d47b223
meri.navicke <meri.navicke@gmail.com>
2021-04-01 21:40:47 +0300

    HW test
    Better used branch but.

--- LearningApp/HomeWork3/ProgramHW3.cs
+++ LearningApp/HomeWork3/ProgramHW3.cs
@@ -4,9 +4,18 @@ namespace LearningApp
 {
     class ProgramHW3
     {
-        static void MainHW3(string[] args)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //**** TASK1
+            Console.WriteLine("Enter three numbers");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            int number3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(number1*number2*number3);
+            
+            //*** TASK2
+
+
         }
     }
 }
--- LearningApp/Lesson1/Program1.cs
+++ LearningApp/Lesson1/Program1.cs
@@ -4,7 +4,7 @@ namespace LearningApp
 {
     class Program1
     {
-        static void Main(string[] args)
+        static void Main1(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
