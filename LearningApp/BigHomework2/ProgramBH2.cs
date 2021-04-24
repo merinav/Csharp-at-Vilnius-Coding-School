afd41db7f0ce3ff2f1b0a196536a962cf2fe0b44
meri.navicke <meri.navicke@gmail.com>
2021-04-24 21:01:46 +0300

    Big HomeWork2 Commit No 3.
    Methods to check if array elements are same

--- LearningApp/BigHomework2/ProgramBH2.cs
+++ LearningApp/BigHomework2/ProgramBH2.cs
@@ -22,28 +22,32 @@ namespace LearningApp.BigHomework2
 
             //IntToIntArray();
 
-            int[] testArray1 = new int[] { 1, 2, 3, 4, 5, 6 };
-            int[] testArray2 = new int[] { 6, 5, 4, 3, 2, 1 };
+            int[] intArray1 = new int[] { 1, 2, 3, 4, 5, 6 };
+            int[] intArray2 = new int[] { 6, 5, 4, 3, 2, 1 };
 
-            Console.WriteLine($"testArray1 is:");
-            Print(testArray1);
+            //Console.WriteLine($"testArray1 is:");
+            //Print(testArray1);
 
-            
-            Console.WriteLine($"is testArray1 made of " +
-                $"different eleements: {ArrayElementsDifferent(testArray1)}");
+            //Console.WriteLine($"testArray2 is:");
+            //Print(testArray2);
 
-            Console.WriteLine($"testArray2 is:");
-            Print(testArray2);
+            //Array1ElementToArray2AllElements(4, intArray2);
 
-            Console.WriteLine($"is testArray2 made of " +
-                $"different eleements: {ArrayElementsDifferent(testArray2)}");
-
-            Console.WriteLine($"are testArray1 and testArray2" +
-               $"made of same elements but all in different places?:" +
-               $" {ArraysSameElementsDifferentPlaces(testArray1, testArray2)}");
+            Console.WriteLine(Array1ElementToArray2AllElements(1, intArray2));
 
 
-            
+            //Console.WriteLine($"is testArray1 made of " +
+            //    $"different eleements: {ArrayElementsDifferent(testArray1)}");        
+
+            //Console.WriteLine($"is testArray2 made of " +
+            //    $"different eleements: {ArrayElementsDifferent(testArray2)}");
+
+            //Console.WriteLine($"are testArray1 and testArray2" +
+            //   $"made of same elements but all in different places?:" +
+            //   $" {ArraysSameElementsDifferentPlaces(testArray1, testArray2)}");
+
+
+
 
         }
 
@@ -52,8 +56,9 @@ namespace LearningApp.BigHomework2
         {
             foreach (int item in array)
             {
-                Console.WriteLine($"<{item}>");
+                Console.Write($"{item} ");                
             }
+            Console.WriteLine();
         }
         
          static void IntToIntArray(int number = 123456)
@@ -100,12 +105,75 @@ namespace LearningApp.BigHomework2
             return returnValue;
         }
 
-         static bool ArraysSameElementsDifferentPlaces(int[] intArray1, int[] intArray2)
+        static bool Array1ElementToArray2AllElements(int array1Element, int[] intArray2) 
         {
-            bool returnValue = true;
+            bool returnValue = false;
+            int array2Element;
+            List<int> tempList = new List<int>();
+            
+            //array copy to list
+            foreach (int item in intArray2)
+            {
+                tempList.Add(item);
+            }
 
+            //for check up
+            Console.WriteLine($"atejes elementas: <{array1Element}>");
+            Console.WriteLine($"atejes masyvas2:");
+            Print(intArray2);
+
+            Console.WriteLine("tempList:");
+            foreach (int item in tempList)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+            
+            //loop to check if array1Element is same as only one of array2 elements (true)
+            for (int i = 0; i < intArray2.Length; i++)
+            {
+                array2Element = intArray2[i];                
+               if (array1Element == array2Element)
+               {
+                    tempList.Remove(tempList[i]);                    
+                    
+                    for (int j = 0; j < tempList.Count; j++)
+                    {
+                        array2Element = tempList[j];
+                        if (array1Element == array2Element)
+                        {
+                            returnValue = false;
+                            break;
+                        }
+                        else
+                        {
+                            returnValue = true;
+                        }
+                    }                 
+               } continue;                
+            }
 
             return returnValue;
         }
+
+        static bool AllArray1ElementsSameAsAllArray2Elements(bool one, bool two, bool three,
+            bool four, bool five, bool six) 
+        {
+            bool returnValue;
+
+            if (one && two && three && four && five && six)
+            {
+                returnValue = true;
+            }
+            else
+            {
+                returnValue = false;
+            }
+
+            return returnValue;
+        }
+
+      
+
     }
 }
