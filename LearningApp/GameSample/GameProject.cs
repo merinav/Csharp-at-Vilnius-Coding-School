74e01087c745279f196bbaef93c4603dd4570e67
meri.navicke <meri.navicke@gmail.com>
2021-04-28 17:27:57 +0300

    GameSample update (lecturer code)2

--- LearningApp/GameSample/GameProject.cs
+++ LearningApp/GameSample/GameProject.cs
@@ -1,5 +1,6 @@
 ﻿using LearningApp.GameSample.Game;
 using LearningApp.GameSample.GUI;
+using LearningApp.GameSample.Windows;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,41 +12,20 @@ namespace LearningApp.GameSample
 {
     class GameProject
     {
-        static void MainGM() 
+        static void Main()
         {
-            GameController gameController = new GameController();
-            gameController.StartGame();
             //GameController gameController = new GameController();
             //gameController.StartGame();
 
             Console.CursorVisible = false;
 
-            Frame frame1 = new Frame(0, 0, 8, 5, '%');
-            frame1.Render();
-            Frame frame2 = new Frame(19, 5, 22, 3, '@');
-            frame2.Render();
-            Frame frame3 = new Frame(29, 9, 22, 10, '#');
-            frame3.Render();
+            MenuWindow gameWindow = new MenuWindow();
+            gameWindow.Render();
 
-            TextLine text1 = new TextLine(1, 2, 6, "Hello!");
-            text1.Render();
-            TextLine text2 = new TextLine(20, 6, 20, "Anybody..");
-            text2.Render();
-            TextLine text3 = new TextLine(30, 10, 20, "There???");
-            text3.Render();
-
-            
-
-            Button button1 = new Button(35, 15, 10, 5, "button");
-            button1.IsActive = true;
-            button1.Render();
-
-            //Window window1 = new Window(0, 0, 60, 25, 'Q');
-            //window1.Render();
+            CreditWindow creditWindow = new CreditWindow();
+            creditWindow.Render();
 
             Console.ReadKey();
-
-
         }
     }
 }
--- LearningApp/GameSample/Windows/MenuWindow.cs
+++ LearningApp/GameSample/Windows/MenuWindow.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LearningApp.GameSample.GUI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,37 @@ using System.Threading.Tasks;
 
 namespace LearningApp.GameSample.Windows
 {
-    class MenuWindow
+    class MenuWindow : Window
     {
+        private Button startButton;
+        private Button creditsButton;
+        private Button quitButton;
+        private TextBlock titleTextBlock;
+
+
+        public MenuWindow() : base(0, 0, 120, 30, "Game menu!", '%')
+        {
+            titleTextBlock = new TextBlock(10, 5, 100, new List<String> { "Super duper zaidimas", "Vardas Pavardaitis kuryba!", "Made in Vilnius Coding School!" });
+
+            startButton = new Button(20, 13, 18, 5, "Start");
+            startButton.SetActive();
+
+            creditsButton = new Button(50, 13, 18, 5, "Credits");
+
+            quitButton = new Button(80, 13, 18, 5, "Quit");
+        }
+
+        public override void Render()
+        {
+            base.Render();
+
+            titleTextBlock.Render();
+
+            startButton.Render();
+            creditsButton.Render();
+            quitButton.Render();
+
+            Console.SetCursorPosition(0, 0);
+        }
     }
 }
