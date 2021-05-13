4c7bb96c09474b26ca24e99b736f014d56f74bc8
meri.navicke <meri.navicke@gmail.com>
2021-05-13 18:30:11 +0300

    DiceMenu Game Commit No 1 - general outline

---
+++ LearningApp/DiceMenu/GUI/GuiObject.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningApp.DiceMenu.GUI
+{
+    abstract class GuiObject
+    {
+
+        //constructor
+        public GuiObject(int x, int y, int width, int height)
+        {
+            X = x;
+            Y = y;
+            Width = width;
+            Heigth = height;
+        }
+
+        //properties
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Width { get; set; }
+        public int Heigth { get; set; }
+
+
+        //methods
+        abstract public void Render();
+
+    }
+}
---
+++ LearningApp/DiceMenu/GUI/Window.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningApp.DiceMenu.GUI
+{
+    class Window : GuiObject
+    {
+        //private fields
+        private Frame border;
+        private TextLine titleText;
+
+        //constructor
+        public Window(int x, int y, int width, int height, string title, char borderChar) : base(x, y, width, height)
+        {
+            border = new Frame(x, y, width, height, borderChar);
+            titleText = new TextLine(x + 3, y, title.Length + 2, $" {title} ");
+        }
+
+        //methods
+        public override void Render()
+        {
+            border.Render();
+            titleText.Render();
+        }
+    }
+}
