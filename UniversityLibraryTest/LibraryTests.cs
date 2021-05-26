49b14e45db16090f229f1c93adf278f063d6e0e3
meri.navicke <meri.navicke@gmail.com>
2021-05-26 20:51:21 +0300

    Testing

---
+++ UniversityLibraryTest/LibraryTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityLibraryTest
+{
+    [TestClass]
+    public class LibraryTests
+    {
+        [TestMethod]
+        public void AddBook() 
+        {
+            Assert.AreEqual(5, 3 + 2, "I was looking for 5");
+        }
+
+    }
+}
