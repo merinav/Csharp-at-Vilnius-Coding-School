59c9b678b3606386fa0746c9f20ee9ac9b9eb7d2
meri.navicke <meri.navicke@gmail.com>
2021-05-26 20:50:57 +0300

    MVC learning app

---
+++ MVC learning app/WebApplication1/WebApplication1/Controllers/DebugController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    public class DebugController : Controller
+    {
+        public string Test()
+        {
+            return "Hello World";
+        }
+
+        public string GetParameter(int id) 
+        {
+            return "I got id: " + id;
+        }
+    }
+}
